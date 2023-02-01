401c688782df9095e6587a0dc20cb3c80f83bd1f
DezBooM <dezboom3@gmail.com>
2023-02-01 13:48:26 +0100

    Minor changes

--- src/components/Checkbox.jsx
+++ src/components/Checkbox.jsx
@@ -1,12 +1,13 @@
 function Checkbox({ id, value, handleChange }) {
   return (
     <div>
-        <input
-            id={id} 
-            type="checkbox" 
-            className="flex justify-center accent-lime-600 w-6 h-6"
-            value={value}
-            onChange={handleChange} />
+      <input
+        id={id}
+        type="checkbox"
+        className="flex justify-center accent-lime-600 w-6 h-6"
+        value={value}
+        onChange={handleChange}
+      />
     </div>
   )
 }
